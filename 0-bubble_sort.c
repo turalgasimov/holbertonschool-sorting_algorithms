f716d9e5795c93c490a52fd446477e127022e51d
turalgasimov <turallgasimov@gmail.com>
2026-02-07 11:46:57 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,8 +20,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				swpd = 1;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
 		if (!swpd)
 			break;
