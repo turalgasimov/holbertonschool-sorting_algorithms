ce437e3c08befc3704c5151c23725c815976550b
turalgasimov <turallgasimov@gmail.com>
2026-02-08 10:59:08 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,7 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+		print_array(array, size);
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
@@ -20,6 +21,5 @@ void selection_sort(int *array, size_t size)
 		temp = array[i];
 		array[i] = array[min];
 		array[min] = temp;
-		print_array(array, size);
 	}
 }
