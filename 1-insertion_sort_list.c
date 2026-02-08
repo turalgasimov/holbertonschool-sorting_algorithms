dfd52b04362b18339ca6a2551a7da68dd2db39f6
turalgasimov <turallgasimov@gmail.com>
2026-02-08 09:20:07 +0000

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,44 +6,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted = NULL;
-	listint_t *curr, *next, *tmp;
+	listint_t *curr, *prev, *next;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	curr = *list;
+
+	curr = (*list)->next;
+
 	while (curr)
 	{
 		next = curr->next;
+		prev = curr->prev;
 
-		/* Fully detach curr */
-		curr->prev = NULL;
-		curr->next = NULL;
-
-		/* Insert at beginning of sorted list */
-		if (sorted == NULL || curr->n <= sorted->n)
+		while (prev && curr->n < prev->n)
 		{
-			curr->next = sorted;
-			if (sorted)
-				sorted->prev = curr;
-			sorted = curr;
+			/* Fix next of prev */
+			prev->next = curr->next;
+			if (curr->next)
+				curr->next->prev = prev;
+
+			/* Move curr before prev */
+			curr->prev = prev->prev;
+			curr->next = prev;
+
+			if (prev->prev)
+				prev->prev->next = curr;
+			else
+				*list = curr;
+
+			prev->prev = curr;
+
+			print_list(*list);
+
+			prev = curr->prev;
 		}
-		else
-		{
-			tmp = sorted;
-			while (tmp->next && tmp->next->n < curr->n)
-				tmp = tmp->next;
-
-			curr->next = tmp->next;
-			if (tmp->next)
-				tmp->next->prev = curr;
-
-			tmp->next = curr;
-			curr->prev = tmp;
-		}
-
-		*list = sorted;
-		print_list(*list);
 		curr = next;
 	}
 }
