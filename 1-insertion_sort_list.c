bf752ec3d59e6d06bf134c462b81b5bb94e6d7e1
turalgasimov <turallgasimov@gmail.com>
2026-02-08 08:32:10 +0000

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - entry point
+ *@list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int key, j;
+	listint_t *l = list;
+
+	l = l->next;
+	while (l)
+	{
+		key = l->n;
+		j = l->previous;
+
+		while (j && (j->n > key))
+		{
+			if (l->next)
+			{
+				j->next = l->next;
+				l->next->previous = j;
+			} else
+			{
+				j->next = NULL;
+			}
+			if (j->previous)
+			{
+				l->previous = j->previous;
+				j->previous->next = l;
+			} else
+			{
+				l->previous = NULL;
+			}
+			j = l->previous;
+		}
+		l = l->next;
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ List of the files and their purposes:
 
 0-bubble_sort.c, 0-O -> sorts an array of integers in ascending order using the Bubble sort algorithm.
 
+1-insertion_sort_list.c, 1-O -> sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm.
